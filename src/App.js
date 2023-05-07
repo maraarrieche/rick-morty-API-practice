34061a69eda078dfbc820e18a91c26d8c6e2efc9
Mariana Arrieche Manzo <maruba@MacBook-Pro-Mariana.local>
2023-05-07 17:17:28 -0400

    my rick and morty api practice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import Filters from "./components/Filters/Filters";
+import Cards from "./components/Cards/Cards";
+import Pagination from './components/Pagination/Pagination';
+import Search from './components/Search/Search';
 
 function App() {
+
+  let [pageNumber, setPageNumber] = useState(1);
+  let [search, setSearch] = useState("");
+
+  let [fetchedData, updateFetchedData] = useState([]);
+  let { info, results } = fetchedData;
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
+
+  useEffect(() => {
+
+    (async function(){
+      let data = await fetch(api).then(res => res.json());
+      updateFetchedData(data)
+    })();
+  }, [api]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      
+      <h1 className="text-center ubuntu my-4">Rick & Morty <span className="text-primary">WiKi</span></h1>
+
+      <Search setPageNumber={setPageNumber} setSearch={setSearch}/>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters />
+          </div>
+          <div className="col-8">
+            <div className="row">
+              <Cards results = {results}/>
+          
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Pagination 
+      info={info}
+      pageNumber={pageNumber} 
+      setPageNumber={setPageNumber}
+      />
+      </div>
   );
 }
 
